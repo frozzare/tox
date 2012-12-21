61db2cb268d7298726c84eedffa0e4e26f3b038f
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2012-12-21 16:08:57 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,29 @@
+var express = require('express')
+  , app = express()
+  , tox = require('./lib/tox');
+  
+tox.setOptions({
+  path: 'test/pages',
+  viewFile: 'page.html'
+});
+
+app.engine('html', require('ejs').renderFile);
+app.set('views', './test/views')
+app.get('/*', tox.page);
+app.listen(4000);
+
+/*
+
+test/pages
+  - index.md
+  - about.md
+  - 404.md
+  - about/contact.md
+  
+urls:
+  - / => index.md
+  - /about => about.md
+  - /about/contact -> about/contact
+  - 404 -> 404.md
+  
+*/
