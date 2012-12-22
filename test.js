a3a6e44bf38a1a4a71031028880cd839f4d68bed
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2012-12-22 23:44:19 +0100

    Mixing test.js code

--- test.js
+++ test.js
@@ -1,19 +1,22 @@
 var express = require('express')
   , app = express()
   , tox = require('./lib/tox');
+
+// Set view engine and directory
+app.engine('html', require('ejs').renderFile);
+app.set('views', './test/views')
   
+// Set tox options
 tox.setOptions({
   path: 'test/pages',
   viewFile: 'page.html',
   viewFiles: {
     'about/*': 'about.html'
-  },
-  debug: true
+  }
 });
 
-app.engine('html', require('ejs').renderFile);
-app.set('views', './test/views')
 app.get('/*', tox.page);
+
 app.listen(4000);
 
 /*
@@ -27,14 +30,7 @@ test/pages
 urls:
   - / => index.md
   - /about => about.md
-  - /about/contact -> about/contact
+  - /about/contact -> about/contact.md
   - 404 -> 404.md
-  
-tox.setOptions({
-  viewFile: {
-    'about/contact': 'about.html',
-    default:
-  }
-});
   
 */
