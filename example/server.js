2a5850a474927dd94efe2cdef940f3716f6a6906
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2012-12-23 13:22:12 +0100

    Deleting comment in server.js

--- example/server.js
+++ example/server.js
@@ -18,19 +18,3 @@ tox.setOptions({
 app.get('/*', tox.page);
 
 app.listen(4000);
-
-/*
-
-test/pages
-  - index.md
-  - about.md
-  - 404.md
-  - about/contact.md
-  
-urls:
-  - / => index.md
-  - /about => about.md
-  - /about/contact -> about/contact.md
-  - 404 -> 404.md
-  
-*/
